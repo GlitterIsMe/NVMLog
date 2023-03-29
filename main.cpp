1086a8a2f72c66236c89bf62d0469543ab4267fd
zhangyiwen <zzyyyww9595@gmail.com>
2023-03-29 19:39:43 +0800

    Add benchmark for PM random write

--- main.cpp
+++ main.cpp
@@ -8,28 +8,45 @@
 #include <cassert>
 
 #include <gflags/gflags.h>
+#include <future>
 
+
+#include "hash.h"
 #include "libpmem.h"
-#include "log.h"
-#include "log_lb.h"
+#include "histogram.h"
 
-DEFINE_string(nvm_path, "/pmem0/log/test_file", "The path of pmem");
+DEFINE_string(nvm_path, "/mnt/pmem0/test_file", "The path of pmem");
 DEFINE_uint64(nvm_size, 10, "The size of pmem <in GB>");
-DEFINE_int32(num_logs, 1, "The number of log");
 DEFINE_int32(num_threads, 1, "The number of thread");
-DEFINE_uint64(nums, 1000000, "The number of operations");
-DEFINE_bool(use_atomic_log, false, "Whether use the atomic based log");
+//DEFINE_uint64(nums, 1000000, "The number of operations");
+DEFINE_uint32(io_size, 1, "The total size of IO that each thread will perform");
 
-DEFINE_int32(value_size, 64, "The size of log entries");
-DEFINE_int64(log_size, 1024 * 1024 * 1024, "The size of log");
+DEFINE_int32(block_size, 16, "The size of log entries");
 
-DEFINE_bool(single_log, false, "Whether use a single log");
+//#define COUNT_LAT
+leveldb::Histogram hist;
+
+inline unsigned long long rdtsc() {
+    unsigned int lo, hi;
+    __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((unsigned long long)hi << 32) | lo;
+}
+
+inline unsigned long long start() {
+    return rdtsc();
+}
+
+inline double get_lat_us(unsigned long long start) {
+    return (double)(rdtsc() - start) / 2.1e6;
+}
 
 int main(int argc, char** argv) {
     google::ParseCommandLineFlags(&argc, &argv, true);
+    printf("threads, block_size\n");
+    printf("%d, %d\n", FLAGS_num_threads, FLAGS_block_size);
+    hist.Clear();
 
-    int log_num = FLAGS_num_logs;
-    int value_size = FLAGS_value_size;
+    int block_size = FLAGS_block_size;
     int thread_num = FLAGS_num_threads;
 
     size_t mapped_len = 0;
@@ -40,99 +57,78 @@ int main(int argc, char** argv) {
 #elif defined(USE_DRAM)
     char* base = new char[FLAGS_nvm_size * 1024 * 1024 *1024];
 #endif
-
-    NVMLog total_log(base, 0, mapped_len);
+    if (base == nullptr) {
+        printf("Map failed for [%s]\n", strerror(errno));
+        exit(-1);
+    }
     std::cout << "mapped len [" << mapped_len << "], is_pmem[" << is_pmem << "]\n";
 
-    // create N log
-    std::vector<NVMLoglb*> loglbs;
-    std::vector<NVMLog*> logs;
-    if (!FLAGS_use_atomic_log) {
-        for (int i = 0; i < log_num; i++) {
-            AllocRes res = total_log.Alloc(FLAGS_log_size);
-            if (res.first == SUCCESS) {
-                std::cout << "Created log at [" << res.second << "]\n";
-                loglbs.push_back(new NVMLoglb(base, res.second, FLAGS_log_size));
-            }
+    uint64_t num_blocks = mapped_len / block_size - 1;
+
+    struct thread_stat {
+        uint64_t op_;
+        uint64_t start_;
+        uint64_t num_blocks_;
+        int id_;
+        char* payload{nullptr};
+        leveldb::Histogram* hist{nullptr};
+
+        thread_stat(int op, uint64_t start, uint64_t nums, int id) {
+            op_ = op;
+            start_ = start;
+            num_blocks_ = nums;
+            id_ = id;
         }
-    } else {
-        for (int i = 0; i < log_num; i++) {
-            AllocRes res = total_log.Alloc(FLAGS_log_size);
-            if (res.first == SUCCESS) {
-                std::cout << "Created log at [" << res.second << "]\n";
-                logs.push_back(new NVMLog(base, res.second, FLAGS_log_size));
-            }
-        }
+    };
+
+    uint64_t per_threads_block_num = num_blocks / FLAGS_num_threads;
+    std::vector<thread_stat> stats;
+    for (int i = 0; i < FLAGS_num_threads; ++i) {
+        stats.emplace_back(FLAGS_io_size * 1024 * 1024 * 1024UL / FLAGS_block_size, i * per_threads_block_num, per_threads_block_num, i);
+        stats[i].payload = new char[FLAGS_block_size];
+        memset((void*)stats[i].payload, 'a', FLAGS_block_size);
+#ifdef COUNT_LAT
+        stats[i].hist = &hist;
+#endif
     }
 
-    // launch N thread to write the log
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> rnd(0, log_num - 1);
+    auto work = [=](thread_stat *stat){
+        for (int i = 0; i < stat->op_; ++i) {
+            int id = i;
+            size_t rand_id = hash::hash_funcs[0](&id, sizeof(id), 0xc70697UL+stat->id_);
+            int rand_blk_id = rand_id % stat->num_blocks_;
+            //if ((base + (stat->start_ + rand_blk_id) * FLAGS_block_size) > (base + mapped_len)) printf("overflow\n");
+#ifdef COUNT_LAT
+            auto t1 = start();
+            pmem_memcpy_persist(base + (stat->start_ + rand_blk_id) * FLAGS_block_size, stat->payload, FLAGS_block_size);
+            hist.Add(get_lat_us(t1));
+#else
+            pmem_memcpy_persist(base + (stat->start_ + rand_blk_id) * FLAGS_block_size, stat->payload, FLAGS_block_size);
+#endif
 
-    // init log entry
-    char* payload = new char[value_size];
-    memset((void*)payload, 0, value_size);
 
-    int op_per_thread = FLAGS_nums / thread_num;
-    if (!FLAGS_use_atomic_log) assert(log_num == 1);
+        }
+        return 0;
+    };
 
-    std::vector<std::thread> threads;
+    std::vector<std::future<int>> ft;
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < thread_num; ++i) {
-        if (!FLAGS_use_atomic_log) {
-            NVMLoglb* target_log = loglbs[0];
-            int thread_count = i;
-            threads.emplace_back(std::thread([&]{
-                uint64_t offset = value_size * thread_count;
-                for (int j = 0; j < op_per_thread; j++, offset + thread_num * value_size) {
-                    target_log->Append(offset, std::string(payload, value_size));
-                }}));
-        } else if (FLAGS_single_log) {
-            NVMLog* target_log = logs[0];
-            threads.emplace_back(std::thread([&]{
-                for (int j = 0; j < op_per_thread; j++) {
-                    std::pair<bool, uint64_t> res = target_log->Alloc(value_size);
-                    if (res.first == SUCCESS) {
-                        target_log->Append(res.second, std::string(payload, value_size));
-                    } else {
-                        std::cout << "Alloc failed, quit\n";
-                        return;
-                    }
-                }}));
-        } else {
-            threads.emplace_back(std::thread([&]{
-                for (int j = 0; j < op_per_thread; j++) {
-                    // select a log in random;
-                    int log_seq = rnd(gen);
-                    // append 64 B to it
-                    NVMLog* target_log = logs[log_seq];
-                    std::pair<bool, uint64_t> res = target_log->Alloc(value_size);
-                    if (res.first == SUCCESS) {
-                        target_log->Append(res.second, std::string(payload, value_size));
-                    } else {
-                        std::cout << "Alloc failed, quit\n";
-                        return;
-                    }
-                }}));
-        }
+        ft.push_back(std::async(work, &stats[i]));
     }
     for (int i = 0; i < thread_num; ++i) {
-        threads[i].join();
+        ft[i].get();
     }
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::micro> elapse = end - start;
 
-    std::cout << "finished " << FLAGS_nums << " operations, throughput: " << FLAGS_nums / elapse.count() << " MOPS\n";
+    std::cout << "each thread finished " << FLAGS_io_size << " GB, throughput: " << (double)FLAGS_io_size * 1024 * 1024 * FLAGS_num_threads / FLAGS_block_size / elapse.count() << " MOPS\n";
+    std::cout << "bandwidth: " << (FLAGS_io_size * FLAGS_num_threads * 1024)/*MB*/ / (elapse.count() / 1000000) << " MB/s\n";
+    printf("%s\n", hist.ToString().c_str());
 
-    delete[] payload;
-    for (int i = 0; i < logs.size(); i++) {
-        delete logs[i];
+    for (int i = 0; i < thread_num; ++i) {
+        delete[] stats[i].payload;
     }
-
-    for (int i = 0; i < loglbs.size(); i++) {
-        delete loglbs[i];
-    }
-
+    pmem_unmap(base, mapped_len);
     return 0;
 }
